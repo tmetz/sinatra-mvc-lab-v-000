a892ff40bc2a73e9678289b309bc0f4c6fce7630
Tammy Metz <tammy@tammymetz.com>
2018-11-12 19:00:02 +0000

    automatically backed up by learn

---
+++ models/piglatinizer.rb
@@ -0,0 +1,20 @@
+class PigLatinizer
+
+  def initialize(phrase)
+    @phrase = phrase
+  end
+
+  def to_pig_latin
+    phrase_array = @phrase.split(" ")
+    latinized = ""
+    phrase_array.each |word| do
+      if word.start_with?("a", "e", "i", "o", "u")
+        word += "way"
+      else
+        word = word[1:] + word[0] + "ay"
+      end
+      latinized += word  
+    end
+    latinized
+  end  
+end
