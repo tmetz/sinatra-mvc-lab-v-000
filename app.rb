fa3fdece8a062cf0cdca9b68f3fedae65e0d2d30
Tammy Metz <tammy@tammymetz.com>
2018-11-12 19:16:17 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,13 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get "/" do
+    erb :user_input
+  end
+  post "/piglatinize" do
+    phrase = params[:user_phrase]
+    piglatin = PigLatinizer.new
+    @latinized = piglatin.piglatinize(phrase)
+    erb :piglatinize
+  end
 end
---
+++ models/piglatinizer.rb
@@ -0,0 +1,20 @@
+class PigLatinizer
+
+  def initialize
+  end
+
+  def piglatinize(phrase)
+    phrase_array = phrase.split(" ")
+    latinized = ""
+    phrase_array.each do |word|
+      if word.start_with?("a", "A", "e", "E", "i", "I", "o", "O", "u", "U")
+        word += "way"
+      else
+        first_chunk = word.split /([aeiou].*)/
+        word = first_chunk[1] + first_chunk[0] + "ay"
+      end
+      latinized += word + " "
+    end
+    latinized[0..-2]
+  end
+end
